02ccf995a50a3fc890cf3ef4f33ada064cafdffa
Sajjad.a <arghaei1380@yahoo.com>
2021-06-01 15:54:26 +0430

    edit pattern and reader classes

--- src/Interpreter/Core/Patterns.java
+++ src/Interpreter/Core/Patterns.java
@@ -1,13 +1,17 @@
 package Interpreter.Core;
 
 public class Patterns {
-    public final static String intRegex = "int[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+=[\\s]+[1-9]+[0_9]{1,7}";
-    public final static String floatRegex = "float[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+=[\\s]+[0-9]+.[0_9]{1,10}";
-    public final static String sumRegex = "[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*[\\s]++[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*";
-    public final static String subRegex = "[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+-[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*";
-    public final static String mulRegex = "[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+*[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*";
-    public final static String divRegex = "[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*[\\s]+-[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*";
-    public final static String printRegex = "print[\\s]+[a-zA-Z]+[a-zA-Z_0-9]*";
+    public final static String intRegex = "int[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[1-9][0_9]{0,7}";
+    public final static String negativeIntRegex = "int[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+-[1-9][0_9]{0,7}";
+    public final static String floatRegex = "float[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[0-9]+.[0_9]{1,10}";
+    public final static String negativeFloatRegex = "float[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+-[0-9]+.[0_9]{1,10}";
+    public final static String floatRegexWithoutPoint = "float[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[0-9]+";
+    public final static String negativeFloatRegexWithoutPoint = "float[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+-[0-9]+";
+    public final static String sumRegex = "[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]++[\\s]+[a-zA-Z][a-zA-Z_0-9]*";
+    public final static String subRegex = "[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+-[\\s]+[a-zA-Z][a-zA-Z_0-9]*";
+    public final static String mulRegex = "[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+*[\\s]+[a-zA-Z][a-zA-Z_0-9]*";
+    public final static String divRegex = "[a-zA-Z][a-zA-Z_0-9]*[\\s]+=[\\s]+[a-zA-Z][a-zA-Z_0-9]*[\\s]+/[\\s]+[a-zA-Z][a-zA-Z_0-9]*";
+    public final static String printRegex = "print[\\s]+[a-zA-Z][a-zA-Z_0-9]*";
     public final static String forStartingRegex = "for[\\s]+[\\d]";
     public final static String forEndingRegex = "end[\\s]+for";
 }
