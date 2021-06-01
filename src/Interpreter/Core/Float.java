0239960ef448b91de77fd82555f3797c18a55339
Amirreza <amirrezasalarimanesh1302@gmail.com>
2021-06-01 17:03:30 +0430

    write setParams Method for Int and Float Classes

--- src/Interpreter/Core/Float.java
+++ src/Interpreter/Core/Float.java
@@ -4,4 +4,20 @@ import java.util.HashMap;
 
 public class Float extends HashMap<String, Double> {
 
+    void setParams(String line) {
+        String newLine;
+        String key;
+        double value;
+        newLine = line.substring(5).trim();
+        int equalIndex = newLine.indexOf('=');
+        if (equalIndex == -1) {
+            key = newLine.trim();
+            value = 0.0;
+        } else {
+            key = newLine.substring(0, newLine.indexOf('=')).trim();
+            value = Double.parseDouble(newLine.substring(newLine.indexOf('=') + 1).trim());
+        }
+
+        this.put(key, value);
+    }
 }
--- src/Interpreter/Core/Int.java
+++ src/Interpreter/Core/Int.java
@@ -4,4 +4,21 @@ import java.util.HashMap;
 
 public class Int extends HashMap<String, Integer> {
 
+    void setParams(String line) {
+        String newLine;
+        String key;
+        int value;
+        newLine = line.substring(3).trim();
+        int equalIndex = newLine.indexOf('=');
+        if (equalIndex == -1) {
+            key = newLine.trim();
+            value = 0;
+        } else {
+            key = newLine.substring(0, newLine.indexOf('=')).trim();
+            value = Integer.parseInt(newLine.substring(newLine.indexOf('=') + 1).trim());
+        }
+
+        this.put(key, value);
+    }
+
 }
