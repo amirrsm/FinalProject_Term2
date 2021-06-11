797f6ffd6882efd6d8da490d9bf726e9e7276e58
Amirreza <amirrezasalarimanesh1302@gmail.com>
2021-06-11 08:54:39 +0430

    create fxmlFile for StoryPage

--- src/Interpreter/Graphics/App.java
+++ src/Interpreter/Graphics/App.java
@@ -1,15 +1,10 @@
 package Interpreter.Graphics;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class App extends Application {
---
+++ src/Interpreter/Graphics/StoryPageController.java
@@ -0,0 +1,13 @@
+package Interpreter.Graphics;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StoryPageController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
